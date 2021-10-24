221a13e17a11fd3beaf5336c3e014a9b4baf774a
WMProject1217 <WMProject1217@outlook.com>
2021-10-24 13:40:21 +0800

    Version 0.0.1145

---
+++ README.md
@@ -0,0 +1,30 @@
+﻿# GameWebsite
+一个基于PHP的游戏中心网站
+A danmaku game center website build with php
+
+实际上作者也不知道其中的大部分游戏的来源，如果你是其中游戏的作者或知道来源的人，请与作者联系以添加你的著作信息或从工程中移除你的游戏。
+In real , the author do not know where much of the games inside is from .
+If you are the author of the games or the human knows the source , please contact the author for add your work info or remove your game from this project .
+
+由于作者正在咕咕咕所以最近可能不怎么更新（
+Because the author is a pigeons so this project will update slow (
+
+协议及条款
+
+GameWebsite ，基于PHP，由 WMProject1217 开发。GameWebsite 及其官方网站版权均归 WMProject1217 所有。
+
+GameWebsite 是非商业许可性质的开源弹幕视频网源码，任何以 GameWebsite 或 WMProject1217 为名义的赞助等均不存在。
+
+如果再混合、转换或者基于本作品进行创作，必须基于与原先许可协议相同的许可协议，分发其贡献的作品。
+
+不得适用法律术语或者技术措施（该协议禁止使用，世界知识产权组织版权公约第11条定义的有效技术措施），从而限制其他人做许可协议允许的事情。
+
+WMProject1217 保留对本条款作出不定时修改的权利。
+
+WMProject1217 对本页面内容拥有最终解释权。
+
+本用户条款最后于 2021/10/24 13:35:19 更新。
+
+赞助
+
+不要相信网络上的任何以 GameWebsite 或 WMProject1217 为名义的赞助。
---
+++ index.php
@@ -0,0 +1,135 @@
+<?php
+echo "<title>主页 - 万恶之源游戏站</title>";
+?>
+<style>
+* {
+    font-family: "Microsoft YaHei","Segoe UI","Lucida Grande",Helvetica,Arial,FreeSans,Arimo,"Droid Sans","wenquanyi micro hei","Hiragino Sans GB","Hiragino Sans GB W3",Arial,sans-serif;
+    box-sizing: border-box;
+}
+
+pre {
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    font-size: 16px;
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+.WMUI_TopBox {
+    position: relative;
+    min-width: 780px;
+    height: 50px;
+    background-color: #000000;
+}
+
+.WMUI_LeftBox {
+    position: relative;
+    float: left;
+    min-width: 270px;
+    max-width: 300px;
+    min-height: 90%;
+    background-color: #66CCFF;
+}
+
+.WMUI_MainBox {
+    position: relative;
+}
+
+.WMUI_MainBox_GameItem {
+    position: relative;
+    float: left;
+    width: 260px;
+    height: 180px;
+}
+
+.WMUI_MainBox_GameItem_Cover {
+    width: 240px;
+    height: 130px;
+}
+</style>
+<div class="WMUI_TopBox">
+    <div style="color: #FFFFFF;">这里有内容，但是没有显示出来。</div>
+</div>
+<div class="WMUI_LeftBox">
+    <pre>
+relative 狂魔（
+
+全是特性（悲
+
+你瞧瞧现在哪里有游戏啊，这都是最近能打开的开源网页游戏，你贤惠我还贤惠呢。
+
+Oops! There must be a lot of features in the project.
+
+好像并没有什么用（确信
+
+WMProject1217 Studios
+项目名称: game.net.wm
+项目备注: 愿在永恒的黑夜来临前能一直伴着你们。
+    </pre>
+</div>
+<div class="WMUI_MainBox">
+    <h3>最近更新</h3>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0000/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0000/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">MikuTap</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0001/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0001/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">WAITING IN LINE 3D</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0002/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0002/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">WAITING IN LINE 3D 离谱版</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0003/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0003/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">Microsoft Edge SURF 2.0</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0004/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0004/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">Minecraft Classic</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0005/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0005/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">人生重开模拟器 1.1</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0006/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0006/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">人生重开模拟器 1.1 破解版</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0007/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0007/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">人生重开模拟器 1.2</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0008/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0008/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">人生重开模拟器 1.2 破解版</div>
+        </a>
+    </div>
+    <div class="WMUI_MainBox_GameItem">
+        <a href="./game/0009/">
+            <img class="WMUI_MainBox_GameItem_Cover" src="./img/0009/cover.png"></img>
+            <div class="WMUI_MainBox_GameItem_Title">Phigros Simulator</div>
+        </a>
+    </div>
+</div>
